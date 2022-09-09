eb1a2430709a8ad606019c2df166d9c1d247566c
Kevin Kaiser <k8x1d@protonmail.ch>
2022-09-09 07:07:43 -0600

    add ivy and language-tool support

---
+++ custom-modules/k8x1d-language-tool.el
@@ -0,0 +1,23 @@
+;;; k8x1d-language-tool.el -- Ivy completion -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022  Kevin Kaiser
+
+;; Author: Kevin Kaiser <k8x1d@proton.me>
+;; Keywords: lisp
+
+(straight-use-package '(eglot-ltex :type git :host github :repo "emacs-languagetool/eglot-ltex"))
+
+(setq eglot-languagetool-server-path "~/Documents/Logiciels/editors_set-up/ltex-ls-15.2.0")
+
+(add-hook 'LaTeX-mode-hook
+          (lambda ()
+            (require 'eglot-ltex)
+            (call-interactively #'eglot)))
+
+(defun kk/start-ltex ()
+  (interactive)
+  (require 'eglot-ltex)
+  (call-interactively #'eglot))
+
+(provide 'k8x1d-language-tool)
+;;; k8x1d-language-tool.el ends here
